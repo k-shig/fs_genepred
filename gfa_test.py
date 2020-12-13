00bdce96a33c9aae537040b394713592a3b9b0aa
k-shig <okurayn@gmail.com>
2020-12-13 16:41:00 +0900

    1213 segfault

--- gfa_test.py
+++ gfa_test.py
@@ -3,8 +3,12 @@
 
 import sys
 import random
+import time
+import warnings
+# warnings.filterwarnings('ignore')
 
-sys.setrecursionlimit(2000)
+sys.setrecursionlimit(2000000)
+sys.settrace 
 
 from test_wiki import *
 from genepred import *
@@ -46,6 +50,7 @@ def readGFAfile(filename):
 
 		# segment lines [seqID, seq]
 		if line_list[0] == 'S':
+			line_list[2].replace("N", "")
 			segment_list.append(line_list[1:])
 
 		# link lines [srcID, +/-, dstID, +/-, cigar ]
@@ -56,62 +61,41 @@ def readGFAfile(filename):
 		else:
 			pass
 
-		gfa_list = [sorted(segment_list, key = lambda x: int(x[0])), sorted(link_list, key = lambda x: int(x[0])), sorted(link_list, key = lambda x: int(x[2]))]
-
+	# gfa_list = [sorted(segment_list, key = lambda x: int(x[0])), sorted(link_list, key = lambda x: int(x[0])), sorted(link_list, key = lambda x: int(x[2]))]
+	gfa_list = [sorted(segment_list, key = lambda x: int(x[0])), sorted(link_list, key = lambda x: int(x[0]))]
 	return gfa_list
 
 # return the list of preceding segment ID(s) of specified segment ID
 def precedingSegmentIDof(gfa_file, segment_id):
 
-	# preSeg(0) = []
-	# preseg(1) = [0]
-	pre_seg_list = [[], [0]]
+	# pre_seg_list = [[], [0]]
 
-	for i in range(len(gfa_file[0])):
-		pre_seg_list.append([])
+	# for i in range(len(gfa_file[0])):
+	# 	pre_seg_list.append([])
 
-	dst_sorted_link_list = gfa_file[2]
+	# sorted_link_list = gfa_file[1]
 
-	for link in dst_sorted_link_list:
-		pre_seg_list[int(link[2])].append(int(link[0]))
+	# for link in sorted_link_list:
+	# 	pre_seg_list[int(link[2])].append(int(link[0]))
 	
-	return pre_seg_list[segment_id]
+	# return pre_seg_list[segment_id]
+	pass
 		
 # return the list of following segment ID(s) of specified segment ID
 def followingSegmentIDof(gfa_file, segment_id):
 
-	# folSeg(0) = [1]
-	fol_seg_list = [[1]]
+	# fol_seg_list = [[1]]
 
-	for i in range(len(gfa_file[0])):
-		fol_seg_list.append([])
+	# for i in range(len(gfa_file[0])):
+	# 	fol_seg_list.append([])
 
-	src_sorted_link_list = gfa_file[1]
+	# sorted_link_list = gfa_file[1]
 
-	for link in src_sorted_link_list:
-		fol_seg_list[int(link[0])].append(int(link[2]))
+	# for link in sorted_link_list:
+	# 	fol_seg_list[int(link[0])].append(int(link[2]))
 
-	return fol_seg_list[segment_id]
-
-gfa_file = readGFAfile("seq.gfa")
-
-# preparing empty lists and a gfa file for running topological sort
-gfa = gfa_file[0] # segment list
-pre  = [] # The list of preceding segment(s)
-fol  = [] # The list of following segment(s)
-tmp  = [] # The list (will be used) to topologically sort the graph 
-data = [] # The list containing data (which will be) used for backtracing
-allpath = [] # The list of all paths
-
-for  i in range(len(gfa)):
-	
-	pre.append(precedingSegmentIDof(gfa_file, i))
-	fol.append(followingSegmentIDof(gfa_file, i))
-	data.append([])
-	allpath.append([])
-
-for i in range(len(pre)):
-	tmp.append(len(pre[i]))
+	# return fol_seg_list[segment_id]
+	pass
 
 def topologicalSort(gfa, start_segment_id, pre, fol, tmp):
 
@@ -283,87 +267,73 @@ def topoSortGenePred(gfa, start_segment_id, hitotsumae, hitotsumae_cnt):
 		topoSortGenePred(gfa, i, hitotsumae, hitotsumae_cnt)
 
 def topoSortGenePred2(gfa, start_segment_id):
-	# print("start : " + str(start_segment_id))
+	
+	if start_segment_id % 10000 == 0:
+		print(start_segment_id)
+		
 	for i in fol[start_segment_id]:
-		# print(i)
+
 		if len(pre[i]) == 0:
-			# tmp[i].append(start_segment_id)
+
 			allpath[i].append(np.array([i]))
-			# print("id : " + str(i))
 
 			if fol[i] == []:
+
 				pass
 
 		elif len(pre[i]) == 1:
-			
-			# tmp[i].append(start_segment_id)
-			# print("start : " + str(start_segment_id))
-			# print("id : " + str(i))
-			# print("pre : 1")
 
 			if pre[i] == [0]: # initial prediction
-
-				data[i].append(viterbi_log_g(start_probability, gfa, i, True))
+				print("initial prediction")
+				data[i].append(viterbi_log_g(start_probability, transition_probability, emission_probability, gfa, i, True))
 				cp = allpath[start_segment_id].copy()
 				allpath[i].append(np.append(cp, np.array([i])))
+				print("initial prediction has ended")
 				pass
 			
 			else:
 				for j in range(len(data[pre[i][0]])):
-					# print("j : " + str(j))
-					data[i].append(viterbi_log_g(data[pre[i][0]][j], gfa, i, False))
+
+					data[i].append(viterbi_log_g(data[pre[i][0]][j], transition_probability, emission_probability, gfa, i, False))
 					cp = allpath[start_segment_id][j].copy()
 					allpath[i].append(np.append(cp, np.array([i])))
-					# print(len(data[i]))
 				pass
-			
-
+		
 		elif len(pre[i]) >= 2: # merging node
-			# print("merging node")
-			# print("id : " + str(i))
-			# print("tmpi = " + str(tmp[i]))
 
-			# if (set(pre[i]) & set(tmp[i])) != set(pre[i]): # totemo omoi
 			if tmp[i] > 1:
 				tmp[i] = tmp[i] - 1
-				# print("Segment ID " + str(i) + " : not all preceding segments are visited, waiting!")
 
-				for j in range(len(data[start_segment_id])):
-					# pass
-					data[i].append(viterbi_log_g(data[start_segment_id][j], gfa, i, False))
-					cp = allpath[start_segment_id][j].copy()
-					allpath[i].append(np.append(cp, np.array([i])))		
+				# for j in range(len(data[start_segment_id])):
+
+				# 	# experimental
+
+				# 	# data[i].append(viterbi_log_g(data[start_segment_id][j], transition_probability, emission_probability, gfa, i, False))
+				# 	# cp = allpath[start_segment_id][j].copy()
+				# 	# allpath[i].append(np.append(cp, np.array([i])))
+
+				# 	pass
 				continue
 				
 			elif tmp[i] == 1:
 
-				# print("Segment ID " + str(i) + " : all preceding segments are visited, go! ")
-				# print("id : " + str(i))
-				# print(pre[i])
-				# print(len(data[i]))
-
 				for j in range(len(data[start_segment_id])):
-					# pass
-					data[i].append(viterbi_log_g(data[start_segment_id][j], gfa, i, False))
+
+					data[i].append(viterbi_log_g(data[start_segment_id][j], transition_probability, emission_probability, gfa, i, False))
 					cp = allpath[start_segment_id][j].copy()
 					allpath[i].append(np.append(cp, np.array([i])))
-				# for j in pre[i]:
-				# 	for k in data[j]:
-				# 		data[i].append(viterbi_log_g(k, gfa, i, False))
-						# cp = allpath[start_segment_id][0].copy()
-						# allpath[i].append(np.append(cp, np.array([i])))
-
-						
-				# print(len(data[i]))
-				# print(data)
-
 				
 				if fol[i] == []:
-					# print("end")
+
 					pass	
-				
-				# continue
-				# print(allpath)
+		
+		# clear the memory
+		if i > 1000 and i % 1000 == 0:
+			data[:i].clear()
+			allpath[:i].clear()
+			pre[:i - 10].clear()
+			fol[:i - 10].clear()
+
 		topoSortGenePred2(gfa, i)
 
 def path2seq(gfa, path):
@@ -374,20 +344,88 @@ def path2seq(gfa, path):
 	return seq
 
 def seq2traceback(seq):
-	return viterbi_log(transition_probability, start_probability, emission_probability, seq2num(seq))
+	return viterbi_log(transition_probability, start_probability, emission_probability, seq2num(seq)).astype(np.int32)
 
+print("Data loading started.")
 
+start = time.time()
+gfa_file = readGFAfile(sys.argv[1])
+end = time.time()
 
+print("Data have loaded. Time taken:" + str(end - start) + " ms")
+
+# preparing empty lists and a gfa file for running topological sort
+gfa = gfa_file[0] # segment list
+pre  = [] # The list of preceding segment(s)
+fol  = [] # The list of following segment(s)
+tmp  = [] # The list (will be used) to topologically sort the graph 
+data = [] # The list containing data (which will be) used for backtracing
+allpath = [] # The list of all paths
+
+# random.shuffle(gfa)
+
+print("Lists preparing started.")
+start = time.time()
+
+for i in range(len(gfa)):
+	pre.append([])
+	fol.append([])
+	data.append([])
+	allpath.append([])
+
+pre[1] = [0]
+fol[0] = [1]
+
+for link in gfa_file[1]:
+	pre[int(link[2])].append(int(link[0]))
+	fol[int(link[0])].append(int(link[2]))
+
+for i in range(len(pre)):
+	tmp.append(len(pre[i]))
+
+end = time.time()
+
+# print(pre[:1000])
+# print(fol[:1000])
+print("Lists have prepared. Time taken:" + str(end - start) + " ms")
+
+start = time.time()
+print("Execution started.")
+
+transition_probability = np.array(
+        [[0.90, 0.10, 0.0, 0.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.95, 0.0, 0.05, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 0.95, 0.0, 0.05, 0.0],
+        [0.01, 0.94, 0.0, 0.0, 0.0, 0.0, 0.05],
+        [0.0, 0.0, 0.01, 0.0, 0.99, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 0.01, 0.0, 0.99, 0.0],
+        [0.0, 0.01, 0.0, 0.0, 0.0, 0.0, 0.99]]
+    )
+
+emission_probability = np.array(
+        [[0.10, 0.10, 0.40, 0.40],
+        [0.30, 0.30, 0.20, 0.20],
+        [0.30, 0.30, 0.20, 0.20],
+        [0.30, 0.30, 0.20, 0.20],
+        [0.10, 0.10, 0.40, 0.40],
+        [0.10, 0.10, 0.40, 0.40],
+        [0.10, 0.10, 0.40, 0.40]]
+    )
 
 topoSortGenePred2(gfa, 0)
+# topoSortGenePred2(gfa, 0) # 1st seg is NNNN...
 
 
 for k in range(len(allpath[-1])):
 	print("path : " + str(allpath[-1][k]))
-	print(path2seq(gfa, allpath[-1][k]))
-	print(data[-1][k])
+	# print(path2seq(gfa, allpath[-1][k]))
+	# print(data[-1][k])
 	print(seq2traceback(path2seq(gfa, allpath[-1][k])))
 	print("\n")
+
+end = time.time()
+
+print("Execution time : " + str(end - start) + " ms")
 
 # print("\n segments yosoku \n")
 
